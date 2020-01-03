6ec58b2556ebbbff62d32fb708a056872aebf643
Hana <hana_369@hotmail.com>
2020-01-04 04:20:58 +0530

    Email Already Existing Exception handling added (does not work). Validations added to the UserDTO. Removed convertor class.

--- src/main/java/lk/apiit/eirlss/bangerandco/repositories/UserRepository.java
+++ src/main/java/lk/apiit/eirlss/bangerandco/repositories/UserRepository.java
@@ -4,7 +4,9 @@ import lk.apiit.eirlss.bangerandco.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, String> {
-    User findUserByEmail(String email);
+    Optional<User> findUserByEmail(String email);
 }
