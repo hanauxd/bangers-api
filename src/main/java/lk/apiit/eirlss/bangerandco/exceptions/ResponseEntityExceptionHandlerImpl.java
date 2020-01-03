1ed98e7ca6e22e8c6a967e83f7f3cf0f66491410
Hana <hana_369@hotmail.com>
2020-01-04 04:11:27 +0530

    Custom exception handling. Unique field doesnt work because the database should be created by JPA however in this case database is already created. Therefore, 'unique = true' doesnt have any effect on the field.

---
+++ src/main/java/lk/apiit/eirlss/bangerandco/exceptions/ResponseEntityExceptionHandlerImpl.java
@@ -0,0 +1,19 @@
+package lk.apiit.eirlss.bangerandco.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+@RestController
+public class ResponseEntityExceptionHandlerImpl extends ResponseEntityExceptionHandler {
+    @ExceptionHandler
+    public final ResponseEntity<Object> handleEmailAlreadyExistException(EmailAlreadyExistException exception, WebRequest request) {
+        EmailAlreadyExistExceptionResponse response = new EmailAlreadyExistExceptionResponse(exception.getMessage());
+        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+    }
+}
