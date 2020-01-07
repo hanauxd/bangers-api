2a3da656b77ddacd47fe8cd6ef93663cb2fc0347
Hana <hana_369@hotmail.com>
2020-01-08 03:21:39 +0530

    findByUser added to the repo.

--- src/main/java/lk/apiit/eirlss/bangerandco/repositories/BookingRepository.java
+++ src/main/java/lk/apiit/eirlss/bangerandco/repositories/BookingRepository.java
@@ -1,6 +1,7 @@
 package lk.apiit.eirlss.bangerandco.repositories;
 
 import lk.apiit.eirlss.bangerandco.models.Booking;
+import lk.apiit.eirlss.bangerandco.models.User;
 import lk.apiit.eirlss.bangerandco.models.Vehicle;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -10,6 +11,6 @@ import java.util.List;
 
 @Repository
 public interface BookingRepository extends JpaRepository<Booking, String> {
-    List<Booking> findByVehicle(Vehicle vehicle);
-    List<Booking> findByVehicleAndStartDateLessThanEqualAndEndDateGreaterThanEqual(Vehicle vehicle, Date endDate, Date startDate);
+    Booking findByVehicleAndStartDateLessThanEqualAndEndDateGreaterThanEqual(Vehicle vehicle, Date endDate, Date startDate);
+    List<Booking> findByUser(User user);
 }
