7e717aa5385525409b9a7227bb7ff224a4c5abf2
Hana <hana_369@hotmail.com>
2020-01-08 03:20:04 +0530

    Create, Delete, get booking by Id and User added.

--- src/main/java/lk/apiit/eirlss/bangerandco/services/BookingService.java
+++ src/main/java/lk/apiit/eirlss/bangerandco/services/BookingService.java
@@ -1,46 +1,85 @@
 package lk.apiit.eirlss.bangerandco.services;
 
 import lk.apiit.eirlss.bangerandco.exceptions.CustomException;
-import lk.apiit.eirlss.bangerandco.models.Booking;
-import lk.apiit.eirlss.bangerandco.models.User;
-import lk.apiit.eirlss.bangerandco.models.Vehicle;
+import lk.apiit.eirlss.bangerandco.models.*;
 import lk.apiit.eirlss.bangerandco.repositories.BookingRepository;
-import lk.apiit.eirlss.bangerandco.repositories.UserRepository;
-import lk.apiit.eirlss.bangerandco.repositories.VehicleRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
 import java.util.List;
 
 @Service
 public class BookingService {
     private BookingRepository bookingRepository;
-    private UserRepository userRepository;
-    private VehicleRepository vehicleRepository;
+    private UserService userService;
+    private VehicleService vehicleService;
+    private UtilityService utilityService;
 
     @Autowired
-    public BookingService(BookingRepository bookingRepository, UserRepository userRepository, VehicleRepository vehicleRepository) {
+    public BookingService(
+            BookingRepository bookingRepository,
+            UserService userService,
+            VehicleService vehicleService,
+            UtilityService utilityService) {
         this.bookingRepository = bookingRepository;
-        this.userRepository = userRepository;
-        this.vehicleRepository = vehicleRepository;
+        this.userService = userService;
+        this.vehicleService = vehicleService;
+        this.utilityService = utilityService;
     }
 
-    public Booking createBooking(Booking booking, String vehicleId, String username) {
+    public Booking createBooking(Booking booking, String vehicleId, List<String> utilities, String username) {
 
-        User user = userRepository.findUserByEmail(username).orElse(null);
-        if (user == null) throw new UsernameNotFoundException("Username '" + username + "' not found.");
+        User user = userService.getUserByEmail(username);
+        Vehicle vehicle = vehicleService.getVehicleById(vehicleId);
 
-        Vehicle vehicle = vehicleRepository.findById(vehicleId).orElse(null);
-        if (vehicle == null) throw new CustomException("Vehicle not found.", HttpStatus.NOT_FOUND);
+        checkVehicleAvailability(vehicle, booking.getEndDate(), booking.getStartDate());
 
-        List<Booking> bookings = bookingRepository.findByVehicleAndStartDateLessThanEqualAndEndDateGreaterThanEqual(vehicle, booking.getEndDate(), booking.getStartDate());
-        if (bookings.size() > 0) throw new CustomException("Vehicle is not available for the selected date range.", HttpStatus.BAD_REQUEST);
+        for (String utilityType : utilities) {
+            Utility utility = utilityService.getUtilityByType(utilityType);
+            BookingUtility bookingUtility = new BookingUtility(booking, utility);
+            booking.getBookingUtilities().add(bookingUtility);
+            utility.getBookingUtilities().add(bookingUtility);
+        }
 
         booking.setVehicle(vehicle);
         booking.setUser(user);
 
         return bookingRepository.save(booking);
+    }
+
+    public Booking updateBooking(Booking booking, String vehicleId) {
+        return null;
+    }
+
+    public List<Booking> getBookingsByUser(User user) {
+        List<Booking> bookings = bookingRepository.findByUser(user);
+        if (bookings.isEmpty()) throw new CustomException("User does not have booking.", HttpStatus.NOT_FOUND);
+        return bookings;
+    }
+
+    public Booking getBookingById(String id) {
+        return bookingRepository.findById(id).orElseThrow(() -> new CustomException("Booking not found.", HttpStatus.NOT_FOUND));
+    }
+
+    public void deleteBooking(String id) {
+        Booking booking = bookingRepository.findById(id).orElse(null);
+
+        if (booking == null) throw new CustomException("Booking not found.", HttpStatus.NOT_FOUND);
+
+        booking.getVehicle().removeBooking(booking);
+        booking.getUser().removeBooking(booking);
+        List<BookingUtility> bookingUtilities = booking.getBookingUtilities();
+        for (BookingUtility bookingUtility : bookingUtilities) {
+            bookingUtility.getUtility().removeBookingUtility(bookingUtility);
+        }
+
+        bookingRepository.delete(booking);
+    }
+
+    private void checkVehicleAvailability(Vehicle vehicle, Date endDate, Date startDate) {
+        Booking booking = bookingRepository.findByVehicleAndStartDateLessThanEqualAndEndDateGreaterThanEqual(vehicle, endDate, startDate);
+        if (booking != null) throw new CustomException("Vehicle is not available for the selected date range.", HttpStatus.BAD_REQUEST);
     }
 }
