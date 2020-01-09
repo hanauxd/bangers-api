46d9e22f3f4e62f8d9f7ce020a78c23e842ce33f
Hana <hana_369@hotmail.com>
2020-01-10 02:32:00 +0530

    Make new booking, update booking and confirm booking added.

--- src/main/java/lk/apiit/eirlss/bangerandco/controllers/BookingController.java
+++ src/main/java/lk/apiit/eirlss/bangerandco/controllers/BookingController.java
@@ -3,9 +3,11 @@ package lk.apiit.eirlss.bangerandco.controllers;
 import lk.apiit.eirlss.bangerandco.dto.requests.BookingDTO;
 import lk.apiit.eirlss.bangerandco.models.Booking;
 import lk.apiit.eirlss.bangerandco.models.User;
+import lk.apiit.eirlss.bangerandco.models.Vehicle;
 import lk.apiit.eirlss.bangerandco.services.BookingService;
 import lk.apiit.eirlss.bangerandco.services.MapValidationErrorService;
 import lk.apiit.eirlss.bangerandco.services.UserService;
+import lk.apiit.eirlss.bangerandco.services.VehicleService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -20,27 +22,30 @@ import java.util.List;
 @RequestMapping("/bookings")
 public class BookingController {
     private BookingService bookingService;
-    private MapValidationErrorService mapValidationErrorService;
     private UserService userService;
+    private VehicleService vehicleService;
+    private MapValidationErrorService mapValidationErrorService;
 
     @Autowired
     public BookingController(
             BookingService bookingService,
             MapValidationErrorService mapValidationErrorService,
-            UserService userService
+            UserService userService,
+            VehicleService vehicleService
     ) {
         this.bookingService = bookingService;
         this.mapValidationErrorService = mapValidationErrorService;
         this.userService = userService;
+        this.vehicleService = vehicleService;
     }
 
     @PreAuthorize("hasAnyRole('USER', 'ADMIN', 'STAFF')")
     @PostMapping
     public ResponseEntity<?> createBooking(@RequestBody BookingDTO dto, Authentication auth, BindingResult result) {
-
         if (result.hasErrors()) return mapValidationErrorService.mapValidationErrorService(result);
-
-        Booking booking = bookingService.createBooking(dto.transformToEntity(), dto.getVehicleId(), dto.getUtilities(), auth.getName());
+        User user = userService.getUserByEmail(auth.getName());
+        Vehicle vehicle = vehicleService.getVehicleById(dto.getVehicleId());
+        Booking booking = bookingService.createBooking(dto.transformToEntity(), vehicle, dto.getUtilities(), user);
         return new ResponseEntity<>(booking, HttpStatus.CREATED);
     }
 
@@ -60,9 +65,23 @@ public class BookingController {
 
     @PreAuthorize("hasAnyRole('USER', 'ADMIN', 'STAFF')")
     @PutMapping("/{id}")
-    public ResponseEntity<?> updateBooking(@PathVariable String id, @RequestBody BookingDTO dto, Authentication auth, BindingResult result) {
-        deleteBooking(id);
-        return createBooking(dto, auth, result);
+    public ResponseEntity<?> updateBooking(@PathVariable String id, @RequestBody BookingDTO dto, BindingResult result) {
+
+        if (result.hasErrors()) return mapValidationErrorService.mapValidationErrorService(result);
+        Vehicle vehicle = vehicleService.getVehicleById(dto.getVehicleId());
+        Booking booking = bookingService.getBookingById(id);
+        dto.updateBooking(booking);
+        List<String> utilities = dto.getUtilities();
+
+        Booking updatedBooking = bookingService.updateBooking(booking, vehicle, utilities);
+        return ResponseEntity.ok(updatedBooking);
+    }
+
+    @PreAuthorize("hasAnyRole('ADMIN', 'STAFF')")
+    @PostMapping("/confirm/{id}")
+    public ResponseEntity<?> confirmBooking(@PathVariable String id, @RequestBody BookingDTO dto) {
+        Booking booking = bookingService.confirmBooking(id, dto.getStatus(), dto.isLateReturn(), dto.getUtilities());
+        return new ResponseEntity<>(booking, HttpStatus.OK);
     }
 
     @PreAuthorize("hasAnyRole('USER', 'ADMIN', 'STAFF')")
