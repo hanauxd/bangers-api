62244c0e13617116f8db69a445b4ad1c70fffd2b
Hana <hana_369@hotmail.com>
2020-02-20 06:38:47 +0530

    VehicleImage entity and repository.

---
+++ src/main/java/lk/apiit/eirlss/bangerandco/models/VehicleImage.java
@@ -0,0 +1,35 @@
+package lk.apiit.eirlss.bangerandco.models;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class VehicleImage {
+    @Id
+    @GeneratedValue(generator = "uuid2")
+    @GenericGenerator(name = "uuid2", strategy = "uuid2")
+    @Column(length = 36)
+    private String id;
+
+    private String filename;
+
+    @ManyToOne(cascade = CascadeType.PERSIST, fetch = FetchType.EAGER)
+    @JoinColumn(name="vehicle", referencedColumnName = "id")
+    @JsonIgnore
+    private Vehicle vehicle;
+
+    public VehicleImage(Vehicle vehicle, String filename) {
+        this.vehicle = vehicle;
+        this.filename = filename;
+        this.vehicle.getVehicleImages().add(this);
+
+    }
+}
---
+++ src/main/java/lk/apiit/eirlss/bangerandco/repositories/VehicleImageRepository.java
@@ -0,0 +1,7 @@
+package lk.apiit.eirlss.bangerandco.repositories;
+
+import lk.apiit.eirlss.bangerandco.models.VehicleImage;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface VehicleImageRepository extends JpaRepository<VehicleImage, String> {
+}
