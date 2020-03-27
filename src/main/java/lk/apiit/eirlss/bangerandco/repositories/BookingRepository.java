c52619afc3e0e124da7ad75662077434632a5dbb
Fathimath Hanau <hanau.xd@gmail.com>
2020-03-27 06:29:36 +0530

    Add pageable to booking findByUser, UserRepository - find user by role and blacklisted.

--- src/main/java/lk/apiit/eirlss/bangerandco/repositories/BookingRepository.java
+++ src/main/java/lk/apiit/eirlss/bangerandco/repositories/BookingRepository.java
@@ -3,6 +3,7 @@ package lk.apiit.eirlss.bangerandco.repositories;
 import lk.apiit.eirlss.bangerandco.models.Booking;
 import lk.apiit.eirlss.bangerandco.models.User;
 import lk.apiit.eirlss.bangerandco.models.Vehicle;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -11,7 +12,7 @@ import java.util.List;
 
 @Repository
 public interface BookingRepository extends JpaRepository<Booking, String> {
-    List<Booking> findByUser(User user);
+    List<Booking> findByUser(User user, Pageable pageable);
     List<Booking> findByUserAndStatus(User user, String status);
     List<Booking> findByStartDateLessThanEqualAndEndDateGreaterThanEqual(Date endDate, Date startDate);
     List<Booking> findByVehicleAndStartDateLessThanEqualAndEndDateGreaterThanEqual(Vehicle vehicle, Date endDate, Date startDate);
--- src/main/java/lk/apiit/eirlss/bangerandco/repositories/UserRepository.java
+++ src/main/java/lk/apiit/eirlss/bangerandco/repositories/UserRepository.java
@@ -4,9 +4,12 @@ import lk.apiit.eirlss.bangerandco.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, String> {
     Optional<User> findUserByEmail(String email);
+    List<User> findUsersByRole(String role);
+    List<User> findUsersByBlacklisted(boolean isBlacklisted);
 }
