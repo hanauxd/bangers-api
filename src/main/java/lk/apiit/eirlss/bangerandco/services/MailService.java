91331c136e4641b6d2eab3cbf262fbafcd442313
Fathimath Hanau <hanau.xd@gmail.com>
2020-05-06 05:06:21 +0530

    Configure email template and render dynamic values

--- src/main/java/lk/apiit/eirlss/bangerandco/services/MailService.java
+++ src/main/java/lk/apiit/eirlss/bangerandco/services/MailService.java
@@ -1,6 +1,10 @@
 package lk.apiit.eirlss.bangerandco.services;
 
+import freemarker.template.Configuration;
+import freemarker.template.Template;
+import freemarker.template.TemplateException;
 import lk.apiit.eirlss.bangerandco.exceptions.CustomException;
+import lk.apiit.eirlss.bangerandco.models.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,16 +14,22 @@ import org.springframework.http.HttpStatus;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
+import org.springframework.ui.freemarker.FreeMarkerTemplateUtils;
 
 import javax.mail.MessagingException;
 import javax.mail.internet.MimeMessage;
 import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
 public class MailService {
     private final Logger LOGGER = LoggerFactory.getLogger(MailService.class);
     private final JavaMailSender sender;
+    private final Configuration configuration;
     private final FileService fileService;
 
     @Value("${dmv.registration-number}")
@@ -28,23 +38,24 @@ public class MailService {
     private String dmvEmail;
 
     @Autowired
-    public MailService(JavaMailSender sender, FileService fileService) {
+    public MailService(JavaMailSender sender, Configuration configuration, FileService fileService) {
         this.sender = sender;
+        this.configuration = configuration;
         this.fileService = fileService;
     }
 
-    public void sendMailWithAttachment(String filename) {
-        MimeMessage message = createMimeMessage(sender.createMimeMessage(), getAttachment(filename));
+    public void sendMailWithAttachment(User user, String filename) {
+        MimeMessage message = createMimeMessage(sender.createMimeMessage(), user, getAttachment(filename));
         sender.send(message);
         LOGGER.debug("Mail sent successfully.");
     }
 
-    private MimeMessage createMimeMessage(MimeMessage message, FileSystemResource file) {
+    private MimeMessage createMimeMessage(MimeMessage message, User user, FileSystemResource file) {
         try {
             MimeMessageHelper helper = new MimeMessageHelper(message, true);
             helper.setTo(dmvEmail);
             helper.setSubject("Reported License");
-            helper.setText(String.format("Registration Number: %s\nDate: %s", registrationNumber, new Date()));
+            helper.setText(htmlContent(user), true);
             helper.addAttachment(file.getFilename(), file);
             return helper.getMimeMessage();
         } catch (MessagingException e) {
@@ -56,5 +67,25 @@ public class MailService {
     private FileSystemResource getAttachment(String filename) {
         String path = fileService.getPathString(filename);
         return new FileSystemResource(new File(path));
+    }
+
+    private String htmlContent(User user) {
+        try {
+            SimpleDateFormat format = new SimpleDateFormat("EEEEE, dd MMM yyyy HH:mm aa");
+            String date = format.format(new Date());
+            Map<String, String> model = new HashMap<>();
+            model.put("registrationNumber", registrationNumber.toUpperCase());
+            model.put("date", date);
+            model.put("customerName", user.getFullName());
+            model.put("license", user.getLicense());
+            model.put("nic", user.getNic());
+
+            configuration.setClassForTemplateLoading(this.getClass(), "/templates");
+            Template template = configuration.getTemplate("email-template.ftl");
+            return FreeMarkerTemplateUtils.processTemplateIntoString(template, model);
+        } catch (IOException | TemplateException e) {
+            LOGGER.warn("Failed to load email template. Error message: {}", e.getMessage());
+            throw new CustomException("Failed to load email template", HttpStatus.BAD_REQUEST);
+        }
     }
 }
